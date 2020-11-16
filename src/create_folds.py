e0143002c8c4f42d7660865863823bffce3bb743
Adarsh Pandey <adarsh.pandey@microfocus.com>
2020-11-17 00:12:26 +0530

    few changes

--- src/create_folds.py
+++ src/create_folds.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn import model_selection
 
 if __name__=='__main__':
-    df = pd.read_csv("F:\AI_repos\EnsembleNStack\data\labeledTrainData.tsv", sep="\t")
+    df = pd.read_csv("data\labeledTrainData.tsv", sep="\t")
     df["kfold"] = -1
 
     df = df.sample(frac=1).reset_index(drop=True)
@@ -15,4 +15,4 @@ if __name__=='__main__':
     for f, (t_,v_) in enumerate(skf.split(X=df, y=y)):
         df.loc[v_,"kfold"]= f
     
-    df.to_csv("F:\AI_repos\EnsembleNStack\data\\train_folds.csv", index=False)
+    df.to_csv("data\\train_folds.csv", index=False)
